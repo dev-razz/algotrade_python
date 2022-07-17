bb433c0321d8c2ed1c9cd287bcbd97b38e20b1f7
Rajat Agrawal <rajatagrawal072000@gmail.com>
2022-07-17 16:48:18 +0530

    initial commit

---
+++ codes/bhavcopy_data_agg.py
@@ -0,0 +1,27 @@
+file_list = glob.glob('path/to/folder/where/files/saved/*.csv')
+#Notice the * which acts as a wildcard.
+#This will give you the path of all files with .csv extension in that folder
+
+
+final_df = pd.DataFrame() #empty dataframe
+
+for csv_file in file_list:
+    df = pd.read_csv(csv_file)
+    csv_file_name = csv_file.split('\\')[7]
+    print('Processing File : {}'.format(csv_file_name))
+    df.columns = df.columns.str.replace(' ', '')
+    df['TIMESTAMP'] = pd.to_datetime(df['TIMESTAMP'])
+    df.set_index(['TIMESTAMP'], inplace=True)
+    
+    if 'Unnamed:13' in df.columns:
+        df.drop(['Unnamed:13'], axis=1, inplace=True)
+   
+    df_trim = df.apply(lambda x: x.str.strip() if x.dtype == 'object' else x)
+
+    new_df = df_trim[df_trim['SERIES'].isin(['EQ', 'BE', 'SM'])]
+    final_df = final_df.append(new_df)
+
+final_df.sort_index(inplace=True) #to sort by dates
+
+
+final_df.to_csv('path/to/folder/bhavcopy_2020_data.csv')
